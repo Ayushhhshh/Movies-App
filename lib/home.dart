9a660ef7785aeaff451e269ad3c8c51e38f6e8f5
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-09-22 23:36:05 +0530

    Home page designing

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,6 +12,16 @@ class HomePage extends StatelessWidget {
         title:  const Text("Netflix", style: TextStyle(color: Colors.white,fontSize: 25,fontFamily: 'Lato',fontWeight: FontWeight.bold),),backgroundColor: const Color.fromARGB(255, 198, 26, 13),
         ),
         backgroundColor: Colors.black,
+        
+        bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.black,
+        color:  Colors.grey,
+        items: 
+      const [
+        Icon(Icons.home),
+        Icon(Icons.search),
+        Icon(Icons.settings),
+      ]),
       );
   }
 }
